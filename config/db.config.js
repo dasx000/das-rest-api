37f8c56838ff4d009ed222ded8c69f0f72620486
dasx000 <dikysaputra2025@gmail.com>
2021-10-29 18:27:04 +0700

    z

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // URL untuk connect ke Mongoodb
+  // URL untuk connect ke Mongoodb Atlas
   // url: `mongodb://localhost:27017/first_api`,
   url: 'mongodb+srv://diky:B5i4FxSIxxDofhCx@cluster0.je1xa.mongodb.net/first_api?retryWrites=true&w=majority&ssl=true',
 };
