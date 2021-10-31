e4a880e00097fda27396c01f663983c3dd8f83e9
dasx000 <dikysaputra2025@gmail.com>
2021-10-31 07:40:27 +0700

    yt

--- app/controllers/tool.controller.js
+++ app/controllers/tool.controller.js
@@ -1,5 +1,7 @@
 const { data, fail } = require('../../message');
 const { scihub } = require('../../lib/scihub');
+const axios = require('axios');
+const { ytMp3, ytMp4, ytPlay } = require('../../lib/youtube');
 
 exports.sci = async (req, res) => {
   const link = req.query.doi;
@@ -10,4 +12,107 @@ exports.sci = async (req, res) => {
   } else {
     res.status(406).send(fail(406, result));
   }
+};
+
+exports.short_url = async (req, res) => {
+  const url = req.query.url;
+  await axios
+    .get(`https://tinyurl.com/api-create.php?url=${url}`)
+    .then((result) => {
+      res.send(data(result.data));
+    })
+    .catch((err) => {
+      // res.status(406).send(fail(406, err));
+      res.status(406).send(fail(406, `Error`));
+    });
+};
+
+exports.ytMP3 = async (req, res) => {
+  const url = req.query.link;
+  await ytMp3(url)
+    .then((results) => {
+      const {
+        title,
+        result,
+        size,
+        thumb,
+        views,
+        likes,
+        dislike,
+        channel,
+        uploadDate,
+        desc,
+      } = results;
+      res.send(
+        data(
+          {
+            title,
+            result,
+            size,
+            thumb,
+            views,
+            likes,
+            dislike,
+            channel,
+            uploadDate,
+            desc,
+          },
+          'Succes. Module by Zekais'
+        )
+      );
+    })
+    .catch((err) => {
+      res.status(406).send(fail(406, 'error'));
+    });
+};
+exports.ytMP4 = async (req, res) => {
+  const url = req.query.link;
+  await ytMp4(url)
+    .then((results) => {
+      const {
+        title,
+        result,
+        quality,
+        size,
+        thumb,
+        views,
+        likes,
+        dislike,
+        channel,
+        uploadDate,
+        desc,
+      } = results;
+      res.send(
+        data(
+          {
+            title,
+            result,
+            quality,
+            size,
+            thumb,
+            views,
+            likes,
+            dislike,
+            channel,
+            uploadDate,
+            desc,
+          },
+          'Succes. Module by Zekais'
+        )
+      );
+    })
+    .catch((err) => {
+      res.status(406).send(fail(406, 'error'));
+    });
+};
+
+exports.ytPLAY = async (req, res) => {
+  const q = req.query.q;
+  await ytPlay(q)
+    .then((result) => {
+      res.send(data(result, 'Succes. Module by Zekais'));
+    })
+    .catch((err) => {
+      res.status(406).send(fail(406, 'error'));
+    });
 };
--- app/routes/tool.route.js
+++ app/routes/tool.route.js
@@ -3,6 +3,10 @@ module.exports = (das) => {
   const router = require('express').Router();
 
   router.get('/scihub', tools.sci);
+  router.get('/short-url', tools.short_url);
+  router.get('/ytmp3', tools.ytMP3);
+  router.get('/ytmp4', tools.ytMP4);
+  router.get('/ytplay', tools.ytPLAY);
 
   das.use('/api/tools', router);
 };
