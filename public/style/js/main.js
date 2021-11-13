c1b7ded1dd350532dfa8f0c7afba98e8d6cb9e93
dasx000 <dikysaputra2025@gmail.com>
2021-11-13 07:44:11 +0700

    p

--- public/style/js/main.js
+++ public/style/js/main.js
@@ -16,23 +16,3 @@ $(document).ready(function () {
     ],
   });
 });
-
-// sweetalert
-const btn = document.querySelector('#confirm-delete-user');
-btn.addEventListener('click', () => {
-  Swal.fire({
-    title: 'Are you sure?',
-    text: "You won't be able to revert this!",
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Yes, delete it!',
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire('Deleted!', 'Your file has been deleted.', 'success');
-    } else {
-      return false;
-    }
-  });
-});
